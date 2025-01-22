6300410069530ec65d7566fd8fa15b3154639aa2
ik1g19 <isaacklugman@gmail.com>
2025-01-22 17:18:45 +0000

    class methods and variables

--- Object Orientation/ClassesAndObjects,.rb
+++ Object Orientation/ClassesAndObjects,.rb
@@ -1,11 +1,21 @@
 class Dog
+
+  # Static variables (class variables) are @@
+  @@num_dogs = 0
+
+  # Static methods
+  def Dog.showInfo
+    puts "This is a class method. Number of dogs = " + @@num_dogs.to_s
+  end
+  
   def talk
-    puts 'Woof! My name is ' + @name
+    puts 'Woof! My name is ' + @name + ' and I am number ' + @@num_dogs.to_s
   end
 
   def initialize( aName )
     # Variables starting with @s are instance variables
     @name = aName
+    @@num_dogs += 1
   end
 end
 
@@ -23,13 +33,18 @@ class GreatPyrenees < Dog
   end
 end
 
+Dog.showInfo
 
 mydog = Dog.new('Bonzo')
-yourdog = Dog.new('Fido')
 
 mydog.talk
+
+yourdog = Dog.new('Fido')
+
 yourdog.talk
 
 pyr = GreatPyrenees.new('Belle', 'wawwawawwawawaa!!!!')
 pyr.talk
 pyr.howl
+
+Dog.showInfo
